83aa6bdadb26d8b822bd6294379b9717ea1cc68d
Lyubomir Georgiev <lyubo12georgiev@gmail.com>
2023-01-11 11:51:40 -0500

    Python Advanced - 01.Lists as Stacks and Queues - Exercises 01 - 02

---
+++ 01-Lists as Stack and Queues/e01_reverse_numbers.py
@@ -0,0 +1,5 @@
+numbers_stack = input().split(" ")
+
+while numbers_stack:
+    current_number = numbers_stack.pop()
+    print(current_number, end=" ")
---
+++ 01-Lists as Stack and Queues/e02_stacked_queries.py
@@ -0,0 +1,5 @@
+n = int(input())
+
+stack = []
+
+for i in range (0, n):
